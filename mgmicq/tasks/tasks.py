66afa90f3df050cfd3f2b2653cfe76182442d30c
Mark Stacy <mbstacy@gmail.com>
2015-03-13 17:35:21 -0500

    Initial MGMIC tasks

---
+++ mgmicq/tasks/tasks.py
@@ -0,0 +1,52 @@
+from celery.task import task
+import paramiko as pk
+import os, json
+
+#Example task
+@task()
+def add(x, y):
+    result = x + y
+    return result
+
+
+@task()
+def qc_docker_workflow(forward_read_filename, reverse_read_filename, basedir="/data/static/",docker_worker='mgmic.oscer.ou.edu'):
+    resultDir = os.path.join(basedir, 'mgmic_tasks/', str(qc_docker_workflow.request.id))
+    os.mkdirs(resultDir)
+    ssh = pk.SSHClient()
+    #keys setup to login as mstacy
+    ssh.connect(docker_worker,22,'mstacy')
+    cmd ='docker run -d -v /opt/local/scripts/:/scripts -v /data/static:/data/static bwawrik/bioinformatics /scripts/bin/Illumina_MySeq_Trim %s %s %s'
+    cmd = cmd % (forward_read_filename,reverse_read_filename,resultDir) 
+    stdin, stdout, stderr = ssh.exec_command(cmd)
+    std_out = stdout.read()
+    std_err = stderr.read()
+    if std_err == '':
+        dock_id = std_out.strip(' \n')
+        #Example poll_docker return
+        #{u'Pid': 0, u'OOMKilled': True, u'Paused': False, u'Running': False,
+        # u'FinishedAt': u'2015-03-13T19:11:31.66940599Z', u'Restarting': False, 
+        # u'Error': u'', u'StartedAt': u'2015-03-13T19:08:56.879774955Z', u'ExitCode': 0}
+        while True:
+            poll = poll_docker(dock_id,ssh)
+            if poll['Running']:
+                sleep(10)
+            else:
+                if poll['ExitCode']==0:
+                    return "http://mgmic.oscer.ou.edu/mgmic_tasks/%s" % (str(qc_docker_workflow.request.id))
+                else:
+                    raise Exception(poll['Error'])
+    else:
+        raise Exception(std_err)
+        
+def poll_docker(docker_id,ssh):
+    cmd = 'docker inspect %s' % (docker_id)
+    stdin, stdout, stderr = ssh.exec_command(cmd)
+    std_out = stdout.read()
+    std_err = stderr.read()
+    if std_err == '':
+        data = json.loads(std_out)
+        return data[0]['State']
+    else:
+        raise Exception(std_err)
+    
