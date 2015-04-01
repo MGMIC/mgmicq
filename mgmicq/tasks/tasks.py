dae9c97732844aa0078f7d3cec54ccb7a08b16e5
Mark Stacy <mbstacy@gmail.com>
2015-04-01 17:31:49 -0500

    Updated task to allow for urls to files instead of local path. This task downloads and places input files in the output directory

--- mgmicq/tasks/tasks.py
+++ mgmicq/tasks/tasks.py
@@ -4,6 +4,7 @@ import paramiko as pk
 import os, json
 import getpass
 from dockertask import docker_task
+from subprocess import call
 
 
 #Example task
@@ -14,109 +15,35 @@ def add(x, y):
 
 
 @task()
-def mgmic_qc_workflow(forward_read_filename, reverse_read_filename, basedir="/data/static/",docker_worker=os.environ['docker_worker']):
-    resultDir = os.path.join(basedir, 'mgmic_tasks/', str(mgmic_qc_workflow.request.id))
+def mgmic_qc_workflow(forward_read_url, reverse_read_url, basedir="/data/static/"):
+    """
+        Task: mgmic_qc_workflow
+        args: [forward_read_url, reverse_read_url]
+        returns: resutl url
+    """
+    task_id = str(mgmic_qc_workflow.request.id
+    resultDir = os.path.join(basedir, 'mgmic_tasks/', task_id)
     os.makedirs(resultDir)
+    for url in [forward_read_url, reverse_read_url]:
+        call['wget','-O',"%s%s" % (resultDir,url.split('/')[-1],url]
     docker_opts = "-v /opt/local/scripts/:/scripts -v /data/static:/data/static"
-    docker_cmd = "/scripts/bin/Illumina_MySeq_Trim %s %s %s" % (forward_read_filename,reverse_read_filename,resultDir)
+    foward_read = "%s%s" % (resultDir,forward_read_url.split('/')[-1])
+    reverse_read = "%s%s" % (resultDir,reverse_read_url.split('/')[-1]) 
+    docker_cmd = "/scripts/bin/Illumina_MySeq_Trim %s %s %s" % (foward_read,reverse_read,resultDir)
     try:
-        result = docker_task(docker_name="bwawrik/bioinformatics",docker_opts=docker_opts,docker_command=docker_cmd,id=str(mgmic_qc_workflow.request.id))
+        result = docker_task(docker_name="bwawrik/bioinformatics",docker_opts=docker_opts,docker_command=docker_cmd,id=task_id)
         return "http://%s/mgmic_tasks/%s" % (result['host'],result['task_id'])
     except:
         raise
 
-def ssh_client(host,port,username):
-    """ Create a paramiko connection object and return it """
-    ssh = pk.SSHClient()
-    ssh.set_missing_host_key_policy(pk.AutoAddPolicy())
-    ssh.connect(host,port,username)
-    return ssh
-
-def docker_command_string(
-        docker_name,
-        docker_opts=None,
-        docker_command=None,
-        docker_command_args=None
-    ):
-    """ Put together a string for running docker """
-    # Set up the docker command line for remote worker
-    if docker_opts == None:
-        docker_run = 'docker run -d %s' % (docker_name)
-    else:
-        docker_run = 'docker run -d %s %s' % (docker_opts, docker_name)
-    # if the command to run is specified overide default for that Dockerfile
-    if docker_command != None:
-        docker_run = docker_run + " " + docker_command
-    return docker_run
-
-def isrunning(state):
-    if state['Running']:
-        return True
-    else:
-        return False
-
-@task
-def remote_task(
-    docker_worker=None,
-    docker_name=None,
-    docker_opts=None,
-    docker_command = None
-    ):
-    """
-    docker_worker => the remote host to run docker container upon
-    docker_name => the docker hub name for the container to run
-    docker_opts => options to docker such as -v --net='host' etc
-    docker_command => the command to run inside the docker
-    """
-    task_id = str(remote_task.request.id)
-
-    try:
-        username = os.environ['docker_username']
-    except:
-        username = getpass.getuser()
-
-    ssh = ssh_client(docker_worker, 22, username)
-
-    if docker_worker == None:
-        try:
-            docker_worker = os.environ['docker_worker']
-        except:
-            print "Please set environtment variable docker_worker="
-
-    if docker_name == None:
-        try:
-            docker_name = os.environ['docker_name']
-        except:
-            print "Please set environment variable docker_name="
-
-    cmd = docker_command_string(
-            docker_name,
-            docker_opts,
-            docker_command)
-
-    stdin, stdout, stderr = ssh.exec_command(cmd)
-    std_err = stderr.read()
-    std_out = stdout.read()
-
-    if std_err == '':
-
-        docker_id = std_out.strip(' \n')
-
-        while True:
-            state = docker_state(docker_id,ssh)
-            if isrunning(state):
-                sleep(5)
-            else:
-                if state['ExitCode']==0:
-                    return { "host": docker_worker, "task_id": str(task_id) }
-                else:
-                    raise Exception(state['Error'])
-    else:
-        raise Exception(std_err)
-
-
 @task()
 def qc_docker_workflow(forward_read_filename, reverse_read_filename, basedir="/data/static/",docker_worker=os.environ['docker_worker']):
+    """
+        Deprecated version of docker_task
+        Task: mgmic_qc_workflow
+        args: [forward_read_filename, reverse_read_filename] This is local paths
+        returns: resutl url
+    """
     resultDir = os.path.join(basedir, 'mgmic_tasks/', str(qc_docker_workflow.request.id))
     os.makedirs(resultDir)
     ssh = pk.SSHClient()
