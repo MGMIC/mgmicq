026a6fd8b7bd7825f73a88b25e34abbe2547ba55
Mark Stacy <mbstacy@gmail.com>
2015-06-30 13:14:30 -0500

    funtional gene workflow

--- mgmicq/tasks/tasks.py
+++ mgmicq/tasks/tasks.py
@@ -7,15 +7,59 @@ import getpass
 from dockertask import docker_task
 from subprocess import call,STDOUT
 from celery.task.sets import TaskSet
-
+import requests
+#Default MGMIC config
+basedir="/data/static/"
 #Example task
 @task()
 def add(x, y):
     result = x + y
     return result
+@task()
+def mgmic_functional_gene(forward_read_url, reverse_read_url, database,result_dir=None,parent_id=None):
+    task_id = str(mgmic_functional_gene.request.id)
+    #Get local database file
+    default_dbs = "http://mgmic.oscer.ou.edu/api/catalog/data/data_portal/gene_database/.json?query=%s"
+    query ="{'spec':{'name':'%s'}}" % database
+    database = requests.get(default_dbs % query).json()
+    database = database['results'][0]
+    db_local_file = database['local_file']
+    #Check Input and setup gene search docker
+    if not result_dir:
+        resultDir = os.path.join(basedir, 'mgmic_tasks/', task_id)
+        os.makedirs(resultDir)
+        #os.chdir(resultDir)
+        #Check if Urls exist
+        if not check_url_exist(forward_read_url):
+            raise Exception("Please Check URL %s" % forward_read_url)
+        if not check_url_exist(reverse_read_url):
+            raise Exception("Please Check URL %s" % reverse_read_url)
+        foward_read = os.path.join(resultDir,forward_read_url.split('/')[-1])
+        reverse_read = os.path.join(resultDir,reverse_read_url.split('/')[-1])
+        logfile= open(resultDir + "/logfile.txt","w")
+        call(['wget','-O',foward_read,forward_read_url],stdout=logfile)
+        call(['wget','-O',reverse_read,reverse_read_url],stdout=logfile)
+        logfile.close()
+    else:
+        resultDir= os.path.join(result_dir,"functional_gene",database.split('.')[0])
+        os.makedirs(resultDir)
+        reverse_read= reverse_read_url
+        foward_read = forward_read_url
+    #Setup Docker container
+    docker_opts = "-v /opt/local/scripts/:/scripts -v /data:/data -v /opt:/opt"
+    docker_cmd = "/scripts/bin/Illumina_MySeq_Quantify_Funtional_Gene.pl %s %s %s %s" % (foward_read,reverse_read,db_local_file,resultDir)
+    try:
+        result = docker_task(docker_name="mgmic/bioinformatics",docker_opts=docker_opts,docker_command=docker_cmd,id=task_id)
+        if parent_id:
+            return "http://%s/mgmic_tasks/%s/%s/%s" % (result['host'],parent_id,"functional_gene",database.split('.')[0])
+        else:
+            return "http://%s/mgmic_tasks/%s" % (result['host'],result['task_id'])
+    except:
+        raise
+
 
 @task()
-def mgmic_16s_classification(forward_read_url, reverse_read_url, basedir="/data/static/",result_dir=None):
+def mgmic_16s_classification(forward_read_url, reverse_read_url, result_dir=None,parent_id=None):
     task_id = str(mgmic_16s_classification.request.id)
     if not result_dir:
         resultDir = os.path.join(basedir, 'mgmic_tasks/', task_id)
@@ -26,8 +70,8 @@ def mgmic_16s_classification(forward_read_url, reverse_read_url, basedir="/data/
             raise Exception("Please Check URL %s" % forward_read_url)
         if not check_url_exist(reverse_read_url):
             raise Exception("Please Check URL %s" % reverse_read_url)
-            foward_read = os.path.join(resultDir,forward_read_url.split('/')[-1])
-            reverse_read = os.path.join(resultDir,reverse_read_url.split('/')[-1])
+        foward_read = os.path.join(resultDir,forward_read_url.split('/')[-1])
+        reverse_read = os.path.join(resultDir,reverse_read_url.split('/')[-1])
         logfile= open(resultDir + "/logfile.txt","w")
         call(['wget','-O',foward_read,forward_read_url],stdout=logfile)
         call(['wget','-O',reverse_read,reverse_read_url],stdout=logfile)
@@ -48,11 +92,14 @@ def mgmic_16s_classification(forward_read_url, reverse_read_url, basedir="/data/
         docker_cmd = "/opt/local/scripts/bin/classify_metagenome_by_16S_step2.pl %s %s %s" % (foward_read,reverse_read,resultDir)
         #result = docker_task(docker_name="bwawrik/qiime",docker_opts=docker_opts,docker_command=docker_cmd,id=task_id)
         result = docker_task(docker_name="qiime_env",docker_opts=docker_opts,docker_command=docker_cmd,id=task_id)
-        return "http://%s/mgmic_tasks/%s" % (result['host'],result['task_id'])
+        if parent_id:
+            return "http://%s/mgmic_tasks/%s/%s" % (result['host'],parent_id,"16s_classification")
+        else:
+            return "http://%s/mgmic_tasks/%s" % (result['host'],result['task_id'])
     except:
         raise
 @task()
-def mgmic_assembly_ray(forward_read_url, reverse_read_url, basedir="/data/static/",result_dir=None):
+def mgmic_assembly_ray(forward_read_url, reverse_read_url, result_dir=None,parent_id=None):
     task_id = str(mgmic_assembly_ray.request.id)
     if not result_dir:
         resultDir = os.path.join(basedir, 'mgmic_tasks/', task_id)
@@ -79,13 +126,16 @@ def mgmic_assembly_ray(forward_read_url, reverse_read_url, basedir="/data/static
     docker_cmd = "/scripts/bin/Illumina_MySeq_Assemble_Ray31.pl %s %s %s" % (forward_read,reverse_read,resultDir)
     try:
         result = docker_task(docker_name="mgmic/bioinformatics",docker_opts=docker_opts,docker_command=docker_cmd,id=task_id)
-        return "http://%s/mgmic_tasks/%s" % (result['host'],result['task_id'])
+        if parent_id:
+            return "http://%s/mgmic_tasks/%s" % (result['host'],parent_id,"assemble_ray")
+        else:
+            return "http://%s/mgmic_tasks/%s" % (result['host'],result['task_id'])
     except:
         raise
 
 
 @task()
-def mgmic_qc_workflow(forward_read_url, reverse_read_url, basedir="/data/static/"):
+def mgmic_qc_workflow(forward_read_url, reverse_read_url,functional_gene=None):
     """
         Task: mgmic_qc_workflow
         args: [forward_read_url, reverse_read_url]
@@ -119,9 +169,13 @@ def mgmic_qc_workflow(forward_read_url, reverse_read_url, basedir="/data/static/
         result = docker_task(docker_name="mgmic/bioinformatics",docker_opts=docker_opts,docker_command=docker_cmd,id=task_id)
         fqc="%s/%s" % (resultDir,"F.QCed.fastq")
         rqc="%s/%s" % (resultDir,"R.QCed.fastq")
-        job = TaskSet(tasks=[ mgmic_assembly_ray.subtask(args=(fqc,rqc),kwargs={'result_dir':resultDir}),
-            mgmic_16s_classification.subtask(args=(fqc,rqc),kwargs={'result_dir':resultDir})
-        ])
+        tasks= [mgmic_assembly_ray.subtask(args=(fqc,rqc),kwargs={'result_dir':resultDir}),
+                mgmic_16s_classification.subtask(args=(fqc,rqc),kwargs={'result_dir':resultDir}),]
+        if functional_gene:
+            for name in functional_gene:
+                tasks.append(mgmic_functional_gene.subtask(args=(fqc, rqc, name),
+                            kwargs={'result_dir':resultDir,'parent_id':task_id}))
+        job = TaskSet(tasks=tasks)
         result_set = job.apply_async()
         #data = result_set.join()
         return "http://%s/mgmic_tasks/%s" % (result['host'],result['task_id'])
@@ -133,52 +187,3 @@ def check_url_exist(url):
     c = httplib.HTTPConnection(p.netloc)
     c.request("HEAD", p.path)
     return c.getresponse().status < 400
-"""
-@task()
-def qc_docker_workflow(forward_read_filename, reverse_read_filename, basedir="/data/static/",docker_worker=os.environ['docker_worker']):
-    ""
-        Deprecated version of docker_task
-        Task: mgmic_qc_workflow
-        args: [forward_read_filename, reverse_read_filename] This is local paths
-        returns: resutl url
-    ""
-    resultDir = os.path.join(basedir, 'mgmic_tasks/', str(qc_docker_workflow.request.id))
-    os.makedirs(resultDir)
-    ssh = pk.SSHClient()
-    ssh.set_missing_host_key_policy(pk.AutoAddPolicy())
-    #keys setup to login as pass environmental username to docker
-    ssh.connect(docker_worker,22,os.environ['username'])
-    cmd ='docker run -d -v /opt/local/scripts/:/scripts -v /data/static:/data/static bwawrik/bioinformatics /scripts/bin/Illumina_MySeq_Trim %s %s %s'
-    cmd = cmd % (forward_read_filename,reverse_read_filename,resultDir)
-    stdin, stdout, stderr = ssh.exec_command(cmd)
-    std_out = stdout.read()
-    std_err = stderr.read()
-    if std_err == '':
-        dock_id = std_out.strip(' \n')
-        #Example poll_docker return
-        #{u'Pid': 0, u'OOMKilled': True, u'Paused': False, u'Running': False,
-        # u'FinishedAt': u'2015-03-13T19:11:31.66940599Z', u'Restarting': False,
-        # u'Error': u'', u'StartedAt': u'2015-03-13T19:08:56.879774955Z', u'ExitCode': 0}
-        while True:
-            poll = docker_state(dock_id,ssh)
-            if poll['Running']:
-                sleep(10)
-            else:
-                if poll['ExitCode']==0:
-                    return "http://%s/mgmic_tasks/%s" % (docker_worker,str(qc_docker_workflow.request.id))
-                else:
-                    raise Exception(poll['Error'])
-    else:
-        raise Exception(std_err)
-
-def docker_state(docker_id,ssh):
-    cmd = 'docker inspect %s' % (docker_id)
-    stdin, stdout, stderr = ssh.exec_command(cmd)
-    std_out = stdout.read()
-    std_err = stderr.read()
-    if std_err == '':
-        data = json.loads(std_out)
-        return data[0]['State']
-    else:
-        raise Exception(std_err)
-"""
