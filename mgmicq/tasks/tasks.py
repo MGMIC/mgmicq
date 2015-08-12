172411683a653170b865fe0e7833419ab25a5743
Mark Stacy <mbstacy@gmail.com>
2015-08-12 13:03:56 -0500

    Updated Amplicon workflow pas map file name not path

--- mgmicq/tasks/tasks.py
+++ mgmicq/tasks/tasks.py
@@ -57,7 +57,7 @@ def amplicon_workflow(forward_read_url, reverse_read_url,mapfile):
         #Step 1
         result = docker_task(docker_name="mgmic/bioinformatics",docker_opts=docker_opts,docker_command=docker_cmd,id=task_id)
         docker_opts = "-i -t -v /opt:/opt -v /data:/data"
-        docker_cmd = "/opt/local/scripts/bin/Illumina_MySeq_16SAmplicon_analysis_part2.pl %s %s %s" % (foward_read,mapfile,resultDir)
+        docker_cmd = "/opt/local/scripts/bin/Illumina_MySeq_16SAmplicon_analysis_part2.pl %s %s %s" % (foward_read,mapfile.split('/')[-1],resultDir)
         #Step 2
         result = docker_task(docker_name="qiime_env",docker_opts=docker_opts,docker_command=docker_cmd,id=task_id)
         return "http://%s/mgmic_tasks/%s" % (result['host'],result['task_id'])
