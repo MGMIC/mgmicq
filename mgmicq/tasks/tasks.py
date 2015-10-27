93f05fc9047750564bc800bf9b1b5888906f5d6e
Mark Stacy <mbstacy@gmail.com>
2015-10-27 16:12:43 -0500

    Updated tasks to include on_off parameter

--- mgmicq/tasks/tasks.py
+++ mgmicq/tasks/tasks.py
@@ -173,7 +173,7 @@ def amplicon_workflow(forward_read_url, reverse_read_url,mapfile,runflags=None):
         raise
 
 @task()
-def mgmic_functional_gene(forward_read_url, reverse_read_url, database,result_dir=None,parent_id=None,runflags=None,workflow=None):
+def mgmic_functional_gene(forward_read_url, reverse_read_url, database,result_dir=None,parent_id=None,runflags=None):
     task_id = str(mgmic_functional_gene.request.id)
     #Get local database file
     default_dbs = "http://mgmic.oscer.ou.edu/api/catalog/data/data_portal/gene_database/.json?query=%s"
@@ -216,7 +216,7 @@ def mgmic_functional_gene(forward_read_url, reverse_read_url, database,result_di
 
 
 @task()
-def mgmic_16s_classification(forward_read_url, reverse_read_url, result_dir=None,parent_id=None,runflags=None,workflow=None):
+def mgmic_16s_classification(forward_read_url, reverse_read_url, result_dir=None,parent_id=None,runflags=None,on_off=None):
     task_id = str(mgmic_16s_classification.request.id)
     if not result_dir:
         resultDir = os.path.join(basedir, 'mgmic_tasks/', task_id)
@@ -256,7 +256,7 @@ def mgmic_16s_classification(forward_read_url, reverse_read_url, result_dir=None
     except:
         raise
 @task()
-def mgmic_assembly_ray(forward_read_url, reverse_read_url, result_dir=None,parent_id=None,runflags=None,workflow=None):
+def mgmic_assembly_ray(forward_read_url, reverse_read_url, result_dir=None,parent_id=None,runflags=None,on_off=None):
     task_id = str(mgmic_assembly_ray.request.id)
     if not result_dir:
         resultDir = os.path.join(basedir, 'mgmic_tasks/', task_id)
@@ -302,30 +302,12 @@ def mgmic_qc_workflow(forward_read_url, reverse_read_url,functional_gene=None,ru
     task_id = str(mgmic_qc_workflow.request.id)
     resultDir = os.path.join(basedir, 'mgmic_tasks/', task_id)
     os.makedirs(resultDir)
-
-    #foward_read = os.path.join(resultDir,forward_read_url.split('/')[-1])
-    #reverse_read = os.path.join(resultDir,reverse_read_url.split('/')[-1])
+    #Log File 
     logfile= open(resultDir + "/logfile.txt","w")
     #setup local files from url or local filename
     foward_read = task_file_setup(forward_read_url,resultDir,logfile)
     reverse_read = task_file_setup(reverse_read_url,resultDir,logfile)
-    #check if local file
-    #if os.path.isfile(forward_read_url):
-    #    os.rename(forward_read_url, os.path.join(resultDir,forward_read_url.split('/')[-1]))
-    #else:
-        #Check if Urls exist
-   #     if not check_url_exist(forward_read_url):
-   #         raise Exception("Please Check URL %s" % forward_read_url)
-   #     call(['wget','-O',foward_read,forward_read_url],stdout=logfile)
-   # if os.path.isfile(reverse_read_url):
-   #     os.rename(reverse_read_url,os.path.join(resultDir,reverse_read_url.split('/')[-1]))
-   # else:
-   #     if not check_url_exist(reverse_read_url):
-   #         raise Exception("Please Check URL %s" % reverse_read_url)
-   #     call(['wget','-O',reverse_read,reverse_read_url],stdout=logfile)
     logfile.close()
-    #docker_opts = "-v /opt/local/scripts/:/scripts -v /data/static:/data/static"
-    #docker_cmd = "/scripts/bin/Illumina_MySeq_Trim %s %s %s" % (foward_read,reverse_read,resultDir)
     try:
         #Step 1 Bioinformatics docker contatiner
         docker_opts = "-v %s:/data -v %s:/scripts" % (docker_config["data_dir"],docker_config["script_dir"])
@@ -339,27 +321,18 @@ def mgmic_qc_workflow(forward_read_url, reverse_read_url,functional_gene=None,ru
         tasks= [mgmic_assembly_ray.subtask(args=(fqc,rqc),kwargs={'result_dir':resultDir,'runflags':runflags,'on_off':workflow.get("assemble","on")}),
                 mgmic_16s_classification.subtask(args=(fqc,rqc),kwargs={'result_dir':resultDir,'runflags':runflags,'on_off':workflow.get("s16","on")}),]
         if functional_gene:
-            #print type(functional_gene)
             for name in functional_gene:
-                tasks.append(mgmic_functional_gene.subtask(args=("%s/%s" % (resultDir,"F.QCed.fasta"),"%s/%s" % (resultDir,"R.QCed.fasta"), name),
+                tasks.append(mgmic_functional_gene.subtask(args=(fqc,rqc, name),
                             kwargs={'result_dir':resultDir,'parent_id':task_id,'runflags':runflags}))
         job = TaskSet(tasks=tasks)
         result_set = job.apply_async()
-        callback = generate_report.subtask(args=(foward_read,reverse_read,task_id,result_set.taskset_id,result_set.subtasks,"callback"),kwargs={'max_retries':2880}).apply_async()
-        #generate_report.subtask(args=(result_set)).apply_async()
+        report_result = generate_report.subtask(args=(foward_read,reverse_read,task_id,result_set.taskset_id,result_set.subtasks,"callback"),
+                            kwargs={'max_retries':2880}).apply_async()
         temp=[]
         for result_d in result_set.subtasks:
-            temp.append(result_d.serializable() )#{"task":"result.task_name","task_id":result_d.id})
-        #report= callback.apply_async()
-        print dir(callback)
-        print temp
-        return {"result_url":"http://%s/mgmic_tasks/%s" % (result['host'],result['task_id']),"subtasks":temp,"report":callback.serializable()}
-        #result_set.taskset_id
-        #result_set.subtasks
-        #if callback is not None:
-        #    subtask(
-        #data = result_set.join()
-        #return "http://%s/mgmic_tasks/%s" % (result['host'],result['task_id'])
+            temp.append({"task":result_d.task_name,"task_id":result_d.id})
+        return {"result_url":"http://%s/mgmic_tasks/%s" % (result['host'],result['task_id']),"subtasks":temp,
+                "report":{"task_id":report_result.id,"task_name":report_result.task_name}}
     except:
         raise
 
